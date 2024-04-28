fdb520f53fdeb851b77fc8ce84602bf66a8290fb
romano1994 <fhaksh0369@gmail.com>
2024-04-28 22:57:50 +0900

    sortnav.vue 추가

---
+++ gs_gg_ui/src/views/characters/CharacterSortNav.vue
@@ -0,0 +1,12 @@
+<template lang="">
+    <div id="sort-winodw">
+        sort nav bar
+    </div>
+</template>
+<script>
+import '@/css/charactersStyle/CharacterSortNav.css';           //css 파일
+export default {
+}
+</script>
+<style>
+</style>
