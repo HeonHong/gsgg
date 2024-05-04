2eed26215101f1e8819c7df72c6e39719a5bdcaa
romano1994 <fhaksh0369@gmail.com>
2024-05-05 00:35:00 +0900

    @Scheduled 시간 수정

--- gs_gg_be/gsggbe/src/main/java/com/gsgg/gsggbe/scheduler/SchedulerJob.java
+++ gs_gg_be/gsggbe/src/main/java/com/gsgg/gsggbe/scheduler/SchedulerJob.java
@@ -15,7 +15,7 @@ public class SchedulerJob {
     private final CrtMainService crtMainService;
 
     //스케줄링 돌아가는 시간 설정, 서버 올라간 후 무조건 돌아가기 때문에 주석 처리
-    @Scheduled(cron = "0 0 22 * * ?")   //매일 오후 10시에 실행
+//    @Scheduled(cron = "0 0 23 * * ?")   //매일 오후 10시에 실행
     public void setCrtInfoToDB() {
         crtMainService.setAPICharacterInfos();
     }
