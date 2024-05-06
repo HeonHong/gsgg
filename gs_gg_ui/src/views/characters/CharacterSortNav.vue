cf468f00474b166301de04f3b8ef52cca3c41ea4
romano1994 <fhaksh0369@gmail.com>
2024-05-06 21:06:50 +0900

    character nav 바 css

--- gs_gg_ui/src/views/characters/CharacterSortNav.vue
+++ gs_gg_ui/src/views/characters/CharacterSortNav.vue
@@ -4,10 +4,9 @@
             <input type="text" id="search-input" placeholder="챔피언 이름 검색(가렌, ㄱㄹ)"/>
         </div>
         <div id="sort-div">
-            <div class="sort-box"
-            v-for="data in this.sort_list" :key="data.key">
-                <input type="radio" name="test" class="test"/>
-                <label for="test">{{data.sort_content}}</label>    
+            <div class="sort-box" v-for="data in this.sort_list" :key="data.key">
+                <input type="radio" name="crt-sort" class="crt-sort" v-model="selectedItem" :value="data" id={{data.key}} @click="testconsole(data.sort_name)"/>
+                <label for="test" id={{data.sort_name}}>{{data.sort_content}}</label>    
             </div>
         </div>
     </div>
@@ -15,6 +14,7 @@
 <script>
 import '@/css/charactersStyle/CharacterSortNav.css';           //css 파일
 export default {
+    //data
     data() {
         return {
             sort_list: [
@@ -23,6 +23,19 @@ export default {
                 {key: 3, sort_name: "eng_name",     sort_content: "영어 이름순 정렬"},
                 {key: 4, sort_name: "eng_name_rev", sort_content: "영어 이름 역순 정렬"},
             ],
+            selectedItem: null,
+        }
+    },
+
+    //mounted
+    mounted() {
+        this.selectedItem = this.sort_list.find(item => item.sort_name === 'kor_name')
+    },
+
+    //methos
+    methods: {
+        testconsole(param) {
+            console.log(param)
         }
     }
 }
