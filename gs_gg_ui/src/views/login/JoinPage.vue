1fb6e254caf9bbaeb052b00244f63e8916ea7419
Heon Hong <ghdgjs3027@gmail.com>
2024-05-10 01:07:26 +0900

    fix. 유효성 검사(수정중)

--- gs_gg_ui/src/views/login/JoinPage.vue
+++ gs_gg_ui/src/views/login/JoinPage.vue
@@ -10,7 +10,7 @@
                     <EpInput v-model="joinData.username" placeholder="아이디를 입력하세요"></EpInput>
                     <EpButton label="중복확인" width="6rem" height="3rem" @click="checkId" />&nbsp; &nbsp; &nbsp;
                 </div>
-                {{ idMessage }}
+                {{ idMsg }}
                 <!-- 비밀번호 정규식 확인 필요 -->
                 <EpPassword v-model="joinData.password" placeholder="비밀번호를 입력하세요"></EpPassword>
                 <EpPassword v-model="pwdCheck" placeholder="비밀번호를 입력하세요"></EpPassword>
@@ -51,39 +51,41 @@ export default {
             isOn: false,
             message: "본인인증 구현 불가(nice api비용처리 불가) 바로 회원가입 하시겠습니까?",
             userCnt: -1,//response값에 따라 재랜더링이 필요하기 때문에 -1로 설정
-            idMessage: "",
+            idMsg: "",
             pwdCheck: "",
+            validation: {
+                id: false,
+                password: false,
+                mobileNum: false,
+            }
         }
     },
     watch: {
         userCnt(newVal) {
-            if (newVal == 0 || newVal == "0") this.idMessage = "사용가능한 아이디입니다."
-            else this.idMessage = "사용 불가능한 아이디입니다."
+            if (newVal == 0 || newVal == "0") {
+                this.idMsg = "사용가능한 아이디입니다."
+                this.validation.id = true;
+            } else this.idMsg = "사용 불가능한 아이디입니다."
+        },
+        'joinData.id'() {
+            this.validation.id = false;
         }
     },
     computed: {
         pwdMsg() {
-            if(this.joinData.password==""){ 
-                    return ""
-            }else{
-                let regex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])\w{8,16}$/g;
-                if(!this.joinData.password.match(regex)){
-                    return "비밀번호는 대소문자,숫자를 포함하여야 합니다."
-                }else{
-    
-                        if(this.pwdCheck != "" ){
-                            if (this.pwdCheck == this.joinData.password) return "비밀번호가 일치합니다"
-                            else return "비밀번호가 일치하지 않습니다."
-        
-                        }else return "" 
-                  
-                } 
-
-            }
+            if(this.valPwd && this.pwdCheck==this.joinData.password ) return "사용 가능한 비밀번호 입니다."
+            else if(!this.valPwd&&this.joinData.password!='') return "비밀번호는 영문 대소문자, 숫자를 포함한 8자리 이상 16자 이하입니다."
+            else if(this.pwdCheck!=""&&this.pwdCheck!=this.joinData.password) return "비밀번호가 다릅니다."
+            else return "" 
+        },
+        valPwd() {
+            let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])\w{8,16}$/g;
+            if (this.joinData.password.match(regex)) return true;
+            else return false;
         }
     },
     methods: {
-        join(){
+        join() {
             this.postApi('/join', this.joinData, this.success, this.fail)
         },
         next() {
@@ -106,10 +108,10 @@ export default {
             this.$router.push('/');
         },
         checkId() {
-            let regex=/^(?=.*[a-z])(?=.*[0-9])\w{8,16}$/g;
-            if(!regex.test(this.joinData.username)){
-                this.idMessage="아이디는 소문자,숫자를 포함한 8글자 이상 16글자 이하로 가능합니다."
-            }else{
+            let regex = /^(?=.*[a-z])(?=.*[0-9])\w{8,16}$/g;
+            if (!regex.test(this.joinData.username)) {
+                this.idMsg = "아이디는 소문자,숫자를 포함한 8글자 이상 16글자 이하로 가능합니다."
+            } else {
                 let param = { username: this.joinData.username };
                 this.getApi('/check-id', param, this.checkSuccess, this.checkFail);
             }
@@ -118,7 +120,7 @@ export default {
             this.userCnt = res.data;
         },
         checkFail() {
-            this.idMessage = '확인 중 오류가 발생하였습니다';
+            this.idMsg = '확인 중 오류가 발생하였습니다';
         },
 
     }
