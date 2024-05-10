266e8c4078b56e825f99f53eddb6aea4f6eaa9f3
HyunwookGil <106861372+HyunwookGil@users.noreply.github.com>
2024-05-10 17:02:46 +0900

    API 테스트 진행중

--- gs_gg_be/gsggbe/src/main/java/com/gsgg/gsggbe/uservs/controller/UserVsController.java
+++ gs_gg_be/gsggbe/src/main/java/com/gsgg/gsggbe/uservs/controller/UserVsController.java
@@ -3,6 +3,7 @@ package com.gsgg.gsggbe.uservs.controller;
 import com.gsgg.gsggbe.uservs.service.UserVsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,17 @@ public class UserVsController {
     private final UserVsService userVsService;
 
     @GetMapping("/getUserInfo")
-    public void getUserInfo() {
+    public void getUserInfo(@RequestParam("mySummonerName") String mySummonerName,
+                            @RequestParam("tagLine") String tagLine) {
+        System.out.println("getUserInfo 확인");
+        this.userVsService.selectUserInfo(mySummonerName, tagLine).subscribe(
+                result -> System.out.println("Response: " + result), // 성공한 경우의 동작
+                error -> System.err.println("Error: " + error) // 에러 발생 시의 동작
+        );
+    }
+
+    @GetMapping("/getUserInfo2")
+    public void getUserInfo2() {
         System.out.println("getUserInfo 확인");
         this.userVsService.selectUserInfo("hide on bush", "KR1").subscribe(
                 result -> System.out.println("Response: " + result), // 성공한 경우의 동작
