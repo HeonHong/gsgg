888c536e4c17336bdb9d363f838633f473aa506a
romano1994 <109515270+Romano1994@users.noreply.github.com>
2024-05-20 19:07:20 +0900

    클라이언트 아이피 확인

--- gs_gg_be/gsggbe/src/main/java/com/gsgg/gsggbe/log/aspect/LogAspect.java
+++ gs_gg_be/gsggbe/src/main/java/com/gsgg/gsggbe/log/aspect/LogAspect.java
@@ -2,13 +2,20 @@ package com.gsgg.gsggbe.log.aspect;
 
 import com.gsgg.gsggbe.log.entity.LogEntity;
 import com.gsgg.gsggbe.mapper.log.LogMapper;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import reactor.netty.http.Cookies;
 
+import java.net.HttpCookie;
+import java.net.InetAddress;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
@@ -17,19 +24,36 @@ import java.util.concurrent.ExecutorService;
 @Component
 @AllArgsConstructor
 @Slf4j
-public class LogAspect {
+public class LogAspect implements HandlerInterceptor {
     private final LogMapper logMapper;
 
     private final ExecutorService executorService;
 
-//    @AfterReturning("execution(* com.gsgg.gsggbe..*(..)) && !execution(* *..log..*(..))")
-    public void logExecution(JoinPoint joinPoint) {
+    @AfterReturning("execution(* com.gsgg.gsggbe..*(..)) && !execution(* *..log..*(..))")
+    public void logExecution(JoinPoint joinPoint, HttpServletRequest request) {
         executorService.submit(() -> {
+            try {
+                InetAddress localHost = InetAddress.getLocalHost();
+                System.out.println("local host==========" + localHost.getHostAddress());
+
+
+//                Cookie[] cookies = request.getCookies();
+//
+//                if(cookies != null) {
+//                    for(Cookie c : cookies) {
+//                        System.out.println(c.getValue());
+//                    }
+//                }
+
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+
             LogEntity logEntity = new LogEntity();
 
-        List<Map<String, Object>> result = logMapper.insertLogTest(logEntity);
+            List<Map<String, Object>> result = logMapper.insertLogTest(logEntity);
 
-        log.info("result==========={}", result);
+            log.info("result==========={}", result);
             log.info("jointPoint============{}", joinPoint.getSignature().getName());
         });
     }
